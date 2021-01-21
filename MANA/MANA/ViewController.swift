2d52e7f4feb600eb7078a9f734a6e5f7e6d91f4d
Miliano Mikol <33736581+mmikol@users.noreply.github.com>
2021-01-21 15:06:08 -0600

    Initial commit

---
+++ MANA/MANA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MANA
+//
+//  Created by Miliano Mikol on 1/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
