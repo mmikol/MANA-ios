a3b3d140dd6b13ad126bde5b9f5b67cc4b21ac6f
Miliano Mikol <33736581+mmikol@users.noreply.github.com>
2021-01-24 15:54:35 -0600

    Added login functions

--- MANA/MANA/LoginViewController.swift
+++ MANA/MANA/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
@@ -14,13 +15,29 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var errorLabel: UILabel!
  
     @IBAction func loginButtonTapped(_ sender: Any) {
+        // TODO: Validate fields same as sign up
+
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                self.errorLabel.text = error!.localizedDescription
+                self.errorLabel.alpha = 1
+            } else {
+                let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.HOME_VIEW_CONTROLLER) as? HomeViewController
+                self.view.window?.rootViewController = homeViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
+        
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpElements()
     }
-    
     
     func setUpElements() {
         errorLabel.alpha = 0
