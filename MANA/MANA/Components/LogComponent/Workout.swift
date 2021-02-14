eefb3f137d17c007b6ffd0744629f164d3c3eac4
Miliano Mikol <33736581+mmikol@users.noreply.github.com>
2021-02-14 16:38:09 -0600

    Working on data persistence

--- MANA/MANA/Components/LogComponent/Workout.swift
+++ MANA/MANA/Components/LogComponent/Workout.swift
@@ -6,17 +6,30 @@
 //
 
 import UIKit
+import os.log
 
-class Workout {
+class Workout : NSObject, NSCoding {
+    //MARK: Properties
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let weight = "weight"
+        static let date = "date"
+        static let photo = "photo"
+    }
+
     var name: String
     var weight: String
     var date: Date
-    var dateString: String
     var photo: UIImage?
+    var dateString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: date)
+    }
     
     init?(name: String, weight: String, date: Date, photo: UIImage?) {
-        
-        guard !name.isEmpty else {
+        guard !name.isEmpty && !weight.isEmpty else {
             return nil
         }
 
@@ -24,11 +37,35 @@ class Workout {
         self.weight = weight
         self.photo = photo
         self.date = date
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        self.dateString = dateFormatter.string(from: date)
-
     }
+    
+    //MARK: NSCoding
+    
+    func encode(with coder: NSCoder) {
+        coder.encode(name, forKey: PropertyKey.name)
+        coder.encode(name, forKey: PropertyKey.weight)
+        coder.encode(name, forKey: PropertyKey.date)
+        coder.encode(name, forKey: PropertyKey.photo)
+    }
+    
+    required convenience init?(coder: NSCoder) {
+        guard let name = coder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Workout object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        guard let weight = coder.decodeObject(forKey: PropertyKey.weight) as? String else {
+            os_log("Unable to decode the name for a Workout object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        guard let date = coder.decodeObject(forKey: PropertyKey.date) as? Date else {
+            os_log("Unable to decode the name for a Workout object.", log: OSLog.default, type: .debug)
+            return nil
+        }
 
+        // let photo = coder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        self.init(name: name, weight: weight, date: date, photo: nil)
+    }
 }
