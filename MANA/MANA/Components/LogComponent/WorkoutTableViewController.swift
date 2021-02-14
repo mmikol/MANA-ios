42a95f355bc018e5cea69b6f8c41f2149ed21187
Miliano Mikol <33736581+mmikol@users.noreply.github.com>
2021-02-14 15:50:17 -0600

    Delete feature added

--- MANA/MANA/Components/LogComponent/WorkoutTableViewController.swift
+++ MANA/MANA/Components/LogComponent/WorkoutTableViewController.swift
@@ -14,7 +14,8 @@ class WorkoutTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationItem.leftBarButtonItem = editButtonItem
+
         guard let workout1 = Workout(name: "Bench", weight: "225", date: Date(), photo: nil) else {
             fatalError("Unable to instnatiate workout1")
         }
@@ -77,25 +78,21 @@ class WorkoutTableViewController: UITableViewController {
 
     }
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            workouts.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
