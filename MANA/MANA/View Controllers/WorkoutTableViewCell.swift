2505b7fbf56b50b565494f6aa813e6b97c5615cb
Miliano Mikol <33736581+mmikol@users.noreply.github.com>
2021-02-13 22:35:48 -0600

    Still stuck with the damn bug

--- MANA/MANA/View Controllers/WorkoutTableViewCell.swift
+++ MANA/MANA/View Controllers/WorkoutTableViewCell.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class WorkoutTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
